031c7f8e2bfeb838effd3ce6894baaa3c31ca1d9
Adilbek Konysbek <akonysbek@micromine.com>
2022-12-20 08:42:32 +0600

    update api.py
    add 404 case and blank tests for character and episode selection

--- api.py
+++ api.py
@@ -3,13 +3,18 @@ import pytest
 
 # BASE_LINK - основная ссылка, по которой будем тестировать API сайте Rick and Morty
 BASE_LINK = "https://rickandmortyapi.com/api/"
-
+BAD_LINK = "https://rickandmortyapi.com/apii/"
 
 @pytest.mark.parametrize('link', ['character', 'location', 'episode'])
 def test_is_200(link):
     res = requests.get(BASE_LINK + link)
     assert res.status_code == 200
 
+# добавил тест на несуществующий линк, мало ли
+# нужно придумать другие пути для тестирования
+def test_is_400():
+    bad_res = requests.get(BAD_LINK)
+    assert bad_res.status_code == 404
 
 @pytest.mark.parametrize('link', ['character', 'location', 'episode'])
 def test_base_api(link):
@@ -34,3 +39,15 @@ def test_locations_api():
 def test_episodes_api():
     ep_res = requests.get(BASE_LINK + "episode")
     assert ep_res.status_code == 200
+
+
+# болванки для тестов выбора персонажа, эпизода,
+# выбора персонажа в эпизоде
+def test_character_selection():
+    pass
+
+def test_episode_selection():
+    pass
+
+def test_char_ep_selection():
+    pass
